1b3da441ee04f08cb3e0e8ef290eef81c85e82a1
Bilal Isa <bilal.isa@pm.me>
2020-05-18 20:15:10 +0200

    master let to const

--- Umbraco.reCAPTCHAv3/src/js/services/recaptchaApi.js
+++ Umbraco.reCAPTCHAv3/src/js/services/recaptchaApi.js
@@ -1,4 +1,4 @@
-﻿let siteVerify = async (response, action) => {
+﻿const siteVerify = async (response, action) => {
     return await fetch(`/umbraco/api/recaptcha/siteverify?recaptchaResponse=${response}&recaptchaAction=${action}`, {
         headers: {"Content-Type": "application/json; charset=utf-8"},
         method: 'POST'
