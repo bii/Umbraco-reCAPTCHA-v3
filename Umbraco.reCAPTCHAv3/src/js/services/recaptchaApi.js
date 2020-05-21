2725eeac97967f6d906862105e9d141e03aebb99
Bilal Isa <bilal.isa@pm.me>
2020-05-21 07:59:58 +0200

    master DeprecationWarning

--- Umbraco.reCAPTCHAv3/src/js/services/recaptchaApi.js
+++ Umbraco.reCAPTCHAv3/src/js/services/recaptchaApi.js
@@ -1,4 +1,4 @@
-﻿const siteVerify = async (response, action) => {
+﻿let siteVerify = async (response, action) => {
     return await fetch(`/umbraco/api/recaptcha/siteverify?recaptchaResponse=${response}&recaptchaAction=${action}`, {
         headers: {"Content-Type": "application/json; charset=utf-8"},
         method: 'POST'
